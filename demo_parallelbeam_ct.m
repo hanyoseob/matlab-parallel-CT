adbd49b56318e15c45edeca21dac2d358b763a39
hanyoseob <hanyoseob@kaist.ac.kr>
2020-01-09 17:40:53 +0900

    update function

--- demo_parallelbeam_ct.m
+++ demo_parallelbeam_ct.m
@@ -5,104 +5,84 @@ close all;
 restoredefaultpath();
 addpath('./lib');
 
-%% Setting the CT System parameters
-% dAngle: Measure from 0 until the angle
+%% Setting the CT System
+% dAngle: Measure from 0 until the angle [degree]
 % nView	: # of the views [unit]
 % dView	: Gap between view_(k) - view_(k-1) [degree]
-param.dAngle        = 180;              
-param.nView         = 90;              % # of elements
+param.dAngle        = 180;  % degree
+param.nView         = 180; 	% # of unit
 param.dView         = param.dAngle/param.nView;  % degree
 param.pdView        = linspace(0, param.dAngle, param.nView + 1);
 param.pdView(end)   = [];
 
-% X-ray source
+%% X-ray source
 % DSO   : Distance from the Source to the Object    [mm]
 % DSD 	: Distance from the Source to the Detector  [mm]
 param.DSO           = 400 ; % mm
 param.DSD           = 800;  % mm
 
-% X-ray detector
-param.dDctX         = 1;    % mm
+%% X-ray detector
+% dDctX             : Detector pitch [mm]
+% nDctX             : Number of detector [element (int)]
+% dOffsetDctX       : Index of shifted detector [element (float)]
+% compute_filtering	: Filtering method, choise=['conv', 'fft']
+param.dDctX         = 0.7;  % mm
 
-param.nDctX         = 185;  % # of elements
+param.nDctX         = 400;  % # of elements
 
 param.dOffsetDctX   = 0; 	% # of elements
 
 param.compute_filtering = 'fft';   % method for computing the filtering function : 'conv', 'fft'
 
-% Object size
+%% Object size
+% dImgY             : Pixel resolution [mm]
+% dImgX             : Pixel resolution [mm]
+% nImgY             : Matrix size of image [element (int)]
+% nImgX             : Matrix size of image [element (int)]
+% dOffsetImgY       : Index of shifted image [element (float)]
+% dOffsetImgX       : Index of shifted image [element (float)]
 param.dImgY         = 1;    % mm
 param.dImgX         = 1;    % mm
 
-param.nImgY         = 128;  % # of elements
-param.nImgX         = 128;  % # of elements
+param.nImgY         = 256;  % # of elements
+param.nImgX         = 256;  % # of elements
 
-param.dOffsetImgX   = 25;    % # of elements
-param.dOffsetImgY	= 25;    % # of elements
+param.dOffsetImgY	= 0;    % # of elements
+param.dOffsetImgX   = 0;    % # of elements
 
-%%
+%% Load image
 load('XCAT512.mat');
-src                 = imresize(XCAT512, [param.nImgY, param.nImgX]);
+input                 = imresize(XCAT512, [param.nImgY, param.nImgX]);
 
-%%
-disp ('built-in ver.');
-
-disp ('projection - built-in');
-tic;
-prj_matlab        	= radon(src, param.pdView);
-toc;
-
-disp ('filtering - built-in');
-tic;
-prj_matlab_flt     	= filterProjections(prj_matlab, 'ram-lak', 1);
-toc;
-
-disp ('backprojection - built-in');
-tic;
-dst_matlab         	= iradon(prj_matlab_flt, param.pdView, 'none', param.nImgY);
-toc;
-
-%%
+%% Run implemented function
 disp ('implementation ver.')
 
 disp ('projection - implementation');
 tic;
-prj_imp         	= projection(src, param);
+prj         = projection(input, param);
 toc;
 
 disp ('filtering - implementation');
 tic;
-prj_imp_flt         = filtering(prj_imp, param);
+prj_flt  	= filtering(prj, param);
 toc;
 
 disp ('backprojection - implementation');
 tic;
-dst_imp             = backprojection(prj_imp_flt, param);
+output   	= backprojection(prj_flt, param);
 toc;
 
-%%
-wndImg      = [0, max(src(:))];
+%% Display reconstruction images
+wndImg      = [0, max(input(:))];
 
-figure(2); colormap gray;
-subplot(331);   imagesc(src, wndImg);     	
+figure('name', 'parallel-beam CT'); colormap gray;
+subplot(2,3,1);     imagesc(input, wndImg);     	
                 axis image;     xlabel('X-axis'); 	ylabel('Y-axis');	title('ground truth');
-subplot(332);   imagesc(dst_matlab, wndImg);
-                axis image;     xlabel('X-axis'); 	ylabel('Y-axis');	title(['reconstruction_{built-in, # of view = ' num2str(param.nView) '}']);
-subplot(333);   imagesc(dst_imp, wndImg);
+subplot(2,3,2);     imagesc(output, wndImg);
                 axis image;     xlabel('X-axis'); 	ylabel('Y-axis');	title(['reconstruction_{implementation, # of view = ' num2str(param.nView) '}']);
-
-subplot(335);   imagesc(src - dst_matlab);
-                axis image;     xlabel('X-axis'); 	ylabel('Y-axis');	title(['difference_{ground truth - built-in, # of view = ' num2str(param.nView) '}']);
-subplot(336);   imagesc(src - dst_imp);
-                axis image;     xlabel('X-axis'); 	ylabel('Y-axis');	title(['difference_{ground truth - implementation, # of view = ' num2str(param.nView) '}']);
-
-subplot(338);   imagesc(prj_matlab);
-                xlabel(['Angle ( \Delta\theta : ' num2str(param.dView) ' \circ )']);	ylabel('Detector');	title(['sinogram_{built-in, # of view = ' num2str(param.nView) '}']);
-                ax              = gca;
-                ax.XTick        = linspace(0, param.nView, 10);
-                ax.XTickLabel	= linspace(0, param.dAngle, 10);
-                
-subplot(339);   imagesc(prj_imp);  
+subplot(2,3,3);     imagesc(input - output);
+                axis image;     xlabel('X-axis'); 	ylabel('Y-axis');	title({'difference', 'ground truch - reconstruction'});
+subplot(2,3,[4,6]); imagesc(prj);  
                 xlabel(['Angle ( \Delta\theta : ' num2str(param.dView) ' \circ )']);	ylabel('Detector');	title(['sinogram_{implementation, # of view = ' num2str(param.nView) '}']);
                 ax              = gca;
                 ax.XTick        = linspace(0, param.nView, 10);
